b3a0a2140bd0e407186a5ac5fe98e315fb0b46ae
Jeff Webb <jeff.webb@codecraftsmen.org>
2022-03-25 10:16:27 -0500

    Fixed bug in pa_mmap_offset for Linux UIO

--- kernel/linux/src/cuddlk_manager_linux.c
+++ kernel/linux/src/cuddlk_manager_linux.c
@@ -129,7 +129,7 @@ static long cuddlk_manager_ioctl(
 				 dev->priv.unique_name,
 				 mslot);
 		} else {
-			mdata->info.priv.pa_mmap_offset = mslot;
+			mdata->info.priv.pa_mmap_offset = mslot * PAGE_SIZE;
 			snprintf(mdata->info.priv.device_name,
 				 CUDDL_MAX_STR_LEN,
 				 "/dev/uio%d",
